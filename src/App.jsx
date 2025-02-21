86d6ca3111d7a6c2175f1a2b9607247f0f26508f
Ankit Deep <AnkitDeep01@outlook.com>
2025-02-21 23:04:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+// import './App.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function App() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
